2d32510cae261cb3e3a67de1008b8cd3f8480915
kdj9878 <kdj9878@gmail.com>
2021-07-28 13:28:17 +0900

    modify

--- js/mouseEvent.js
+++ js/mouseEvent.js
@@ -7,8 +7,16 @@ const text2 = document.querySelector(".showText2");
 const text3 = document.querySelector(".showText3");
 
 
+const gif1 = document.querySelector(".gif1");
+const gif2 = document.querySelector(".gif2");
+
+const p_text = document.querySelector(".p_text");
+const p_text2 = document.querySelector(".p_text2");
+
+
 function showText(event){
     const targetId = event.target.id;
+    console.log(targetId);
     if(targetId==='img1'){
         text1.style.display = 'block';
     }
@@ -33,6 +41,30 @@ function hiddenText(event){
     }
 }
 
+
+function showText2(event){
+    const targetId3 = event.target.id;
+    console.log(targetId3)
+    if(targetId3==='gif1'){
+        p_text.style.display = 'block';
+    }
+    if(targetId3==='gif2'){
+        p_text2.style.display = 'block';
+    }
+
+}
+
+function hiddenText2(event){
+    const targetId4 = event.target.id;
+    if(targetId4==='gif1'){
+        p_text.style.display = 'none';
+    }
+    if(targetId4==='gif2'){
+        p_text2.style.display = 'none';
+    }
+
+}
+
 img1.addEventListener("mouseover", showText);
 img1.addEventListener("mouseout", hiddenText);
 img2.addEventListener("mouseover", showText);
@@ -40,3 +72,7 @@ img2.addEventListener("mouseout", hiddenText);
 img3.addEventListener("mouseover", showText);
 img3.addEventListener("mouseout", hiddenText);
 
+gif1.addEventListener("mouseover", showText2);
+gif1.addEventListener("mouseout", hiddenText2);
+gif2.addEventListener("mouseover", showText2);
+gif2.addEventListener("mouseout", hiddenText2);
